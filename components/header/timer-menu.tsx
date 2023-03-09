d43c94884553dd8ad3a34cbaa0da23205265e61a
nikagoci <dato.svanidze2005@mail.ru>
2023-03-09 22:39:07 +0400

    add timer functional(can down count on all of state)

--- components/header/timer-menu.tsx
+++ components/header/timer-menu.tsx
@@ -1,5 +1,5 @@
 import { BsFillSkipEndFill } from "react-icons/bs";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
 import { focusEnum } from "@/pages";
 import TimerStart from "../shared/timer-start";
@@ -9,7 +9,64 @@ interface Props {
   focus: focusEnum;
 }
 
+const TIME_VALUES_FOR_FOCUS = {
+  pomodoro: "25:00",
+  short: "5:00",
+  long: "15:00",
+};
+
 export default function TimerMenu({ setFocus, focus }: Props) {
+  const [isTimerStarted, setIsTimerStarted] = useState(false);
+  const [curTime, setCurTime] = useState("25:00");
+
+  useEffect(() => {
+    setCurTime(TIME_VALUES_FOR_FOCUS[focus]);
+  }, [focus]);
+
+  useEffect(() => {
+    let timer: ReturnType<typeof setInterval>;
+    let point = 0
+    if (isTimerStarted) {
+      timer = setInterval(() => {
+        const minutes = parseInt(curTime.split(":")[0]);
+        const seconds = parseInt(curTime.split(":")[1]);
+
+        if (minutes === 0 && seconds === 0) {
+          clearInterval(timer);
+          setIsTimerStarted(false);
+          setCurTime(TIME_VALUES_FOR_FOCUS[focus]);
+
+        } else {
+          if (minutes < 10) {
+            if (seconds === 0) {
+              setCurTime(`0${minutes - 1}:59`);
+            } else {
+              setCurTime(
+                `0${minutes}:${seconds - 1 < 10 ? "0" : ""}${seconds - 1}`
+              );
+            }
+          } else {
+            if (seconds === 0) {
+              setCurTime(`${minutes - 1}:59`);
+            } else {
+              setCurTime(
+                `${minutes}:${seconds - 1 < 10 ? "0" : ""}${seconds - 1}`
+              );
+            }
+          }
+        }
+      }, 1000);
+    }
+
+
+    return () => clearInterval(timer);
+  }, [curTime, isTimerStarted]);
+
+  useEffect(() => {
+    setCurTime(TIME_VALUES_FOR_FOCUS[focus]);
+    setIsTimerStarted(false);
+  }, [focus]);
+
   const compareFocuses = (curFocus: focusEnum): string => {
     if (focus === curFocus) {
       return "bg-transparent-white";
@@ -47,14 +104,21 @@ export default function TimerMenu({ setFocus, focus }: Props) {
         </div>
       </div>
       <div>
-        <div className="py-6 font-bold text-center text-8xl">25:00</div>
+        <div className="py-6 font-bold text-center text-8xl">{curTime}</div>
       </div>
       <div className="relative flex justify-center">
-        <TimerStart focus={focus} />
-        <BsFillSkipEndFill
-          size={40}
-          className="absolute duration-200 transform -translate-y-1/2 cursor-pointer top-1/2 sm:right-20 right-14 hover:opacity-20"
+        <TimerStart
+          focus={focus}
+          setIsTimerStarted={setIsTimerStarted}
+          isTimerStarted={isTimerStarted}
         />
+        {isTimerStarted && (
+          <BsFillSkipEndFill
+            size={40}
+            className="absolute duration-200 transform -translate-y-1/2 cursor-pointer top-1/2 sm:right-20 right-14 hover:opacity-20"
+            onClick={() => setCurTime("00:00")}
+          />
+        )}
       </div>
     </div>
   );
--- components/shared/timer-start.tsx
+++ components/shared/timer-start.tsx
@@ -1,13 +1,29 @@
-import { focusEnum } from "@/pages"
+import { Dispatch, SetStateAction } from "react";
+
+import { focusEnum } from "@/pages";
 
 interface Props {
-    focus: focusEnum
+  focus: focusEnum;
+  setIsTimerStarted: Dispatch<SetStateAction<boolean>>;
+  isTimerStarted: boolean;
 }
 
-export default function PauseButton({focus}: Props){
-    return (
-        <button className={`px-12 py-3 text-2xl font-semibold bg-white rounded ${focus === focusEnum.pomodoro ? 'text-primary': ''} ${focus === focusEnum.short ? 'text-secondary' : ''} ${focus === focusEnum.long ? 'text-tertiary' : ''}`}>
-            START
-        </button>
-    )
+export default function PauseButton({
+  focus,
+  setIsTimerStarted,
+  isTimerStarted,
+}: Props) {
+
+  return (
+    <button
+      className={`transform duration-200 ${!isTimerStarted ? 'button-shadow' : ''} active:shadow-none px-12 py-3 text-2xl font-semibold bg-white rounded ${
+        focus === focusEnum.pomodoro ? "text-primary" : ""
+      } ${focus === focusEnum.short ? "text-secondary" : ""} ${
+        focus === focusEnum.long ? "text-tertiary" : ""
+      }`}
+      onClick={() => setIsTimerStarted(prev => !prev)}
+    >
+      {isTimerStarted ? 'PAUSE' : 'START'}
+    </button>
+  );
 }
