839ca78a18bf73cd32f4830bc8fa17e7b5e3f4fb
nikagoci <dato.svanidze2005@mail.ru>
2023-03-09 15:26:23 +0400

    add focus state for controlling three main focus state

--- components/header/timer-menu.tsx
+++ components/header/timer-menu.tsx
@@ -1,33 +1,61 @@
 import { BsFillSkipEndFill } from "react-icons/bs";
+import { Dispatch, SetStateAction } from "react";
 
-
-import MenuButton from "../shared/menu-button";
+import { focusEnum } from "@/pages";
 import PauseButton from "../shared/pause-button";
-import FocusTime from "./focus_time";
-import Tasks from "./tasks";
 
-export default function TimerMenu() {
+interface Props {
+  setFocus: Dispatch<SetStateAction<focusEnum>>;
+  focus: focusEnum;
+}
+
+export default function TimerMenu({ setFocus, focus }: Props) {
+  const compareFocuses = (curFocus: focusEnum): string => {
+    if (focus === curFocus) {
+      return "bg-transparent-white";
+    }
+
+    return "";
+  };
+
   return (
-      <div className="flex flex-col bg-transparent-black p-7 rounded mt-8 space-y-4 max-w-[550px] mx-auto">
-        <div className="flex items-center justify-center font-semibold cursor-pointer">
-          <div className="px-3 py-1 rounded-md bg-transparent-white ">
-            Pomodoro
-          </div>
-          <div className="px-3 py-1 rounded-md">Short Break</div>
-          <div className="px-3 py-1 rounded-md">Long Break</div>
+    <div className="flex flex-col bg-transparent-black p-7 rounded mt-8 space-y-4 max-w-[550px] mx-auto">
+      <div className="flex items-center justify-center font-semibold cursor-pointer">
+        <div
+          className={`px-3 py-1 rounded-md transform duration-200 ${compareFocuses(
+            focusEnum.pomodoro
+          )} `}
+          onClick={() => setFocus(focusEnum.pomodoro)}
+        >
+          Pomodoro
         </div>
-        <div>
-          <div className="py-6 font-bold text-center text-8xl">25:00</div>
+        <div
+          className={`px-3 py-1 rounded-md transform duration-200 ${compareFocuses(
+            focusEnum.short
+          )}`}
+          onClick={() => setFocus(focusEnum.short)}
+        >
+          Short Break
         </div>
-        <div className="relative flex justify-center">
-          <PauseButton />
-          <BsFillSkipEndFill
-            size={40}
-            className="absolute duration-200 transform -translate-y-1/2 cursor-pointer top-1/2 sm:right-20 right-14 hover:opacity-20"
-          />
+        <div
+          className={`px-3 py-1 rounded-md transform duration-200 ${compareFocuses(
+            focusEnum.long
+          )}`}
+          onClick={() => setFocus(focusEnum.long)}
+        >
+          Long Break
         </div>
       </div>
-
-
+      <div>
+        <div className="py-6 font-bold text-center text-8xl">25:00</div>
+      </div>
+      <div className="relative flex justify-center">
+        <PauseButton />
+        <BsFillSkipEndFill
+          size={40}
+          className="absolute duration-200 transform -translate-y-1/2 cursor-pointer top-1/2 sm:right-20 right-14 hover:opacity-20"
+        />
+      </div>
+    </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,21 @@ import FocusTime from "@/components/header/focus_time";
 import Tasks from "@/components/header/tasks";
 import TimerMenu from "@/components/header/timer-menu";
 
+import { useState } from "react";
+
+export enum focusEnum {
+  pomodoro = 'pomodoro',
+  short = 'short',
+  long = 'long'
+}
+
 export default function Home() {
+  const [ focus, setFocus ] = useState<focusEnum>(focusEnum.pomodoro)
+
   return (
     <div className="text-white w-[90%] mx-auto flex flex-col">
       <div>
-        <TimerMenu />
+        <TimerMenu setFocus={setFocus} focus={focus} />
         <FocusTime />
         <Tasks />
       </div>
